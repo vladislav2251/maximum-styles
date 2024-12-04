43d909d3df94beae2ff4bf66de6e75543caca5dc
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-12-04 18:00:06 +0100

    feat: trnaslation for the checkout

--- src/lib/components/sections/checkout/checkout.svelte
+++ src/lib/components/sections/checkout/checkout.svelte
@@ -3,6 +3,7 @@
   export let account;
   export let cart;
   export let price;
+  export let translation;
   let { email, personal_data, delivery_data } = account;
 
   const cartComponents = cart.map((item) => {
@@ -47,56 +48,63 @@
     {'←'}
   </button>
 
-  <h1 class="text-3xl font-bold text-center mb-8">Checkout</h1>
+  <h1 class="text-3xl font-bold text-center mb-8">
+    {translation?.checkout?.title}
+  </h1>
 
   <form class="flex flex-col gap-4" on:submit|preventDefault={onFormSubmit}>
     <fieldset
       class="p-6 bg-white rounded-md shadow hover:shadow-lg transition-all duration-200"
     >
-      <legend class="text-2xl font-semibold mb-4">User Information</legend>
+      <legend class="text-2xl font-semibold mb-4"
+        >{translation?.checkout?.user_info}</legend
+      >
       <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <label for="email" class="block text-gray-700">
-          Email:
+          {translation?.checkout?.inputs[0]?.label}:
           <input
             required
             type="email"
             id="email"
             name="email"
             bind:value={email}
+            placeholder={translation?.checkout?.inputs[0]?.placeholder}
             class="px-8 py-4 border-solid outline-none text-base w-full border font-normal border-[var(--color-gray)] rounded-md transition-all duration-300 focus:border-[var(--color-primary-300)];"
           />
         </label>
         <label for="first_name" class="block text-gray-700">
-          First Name:
+          {translation?.checkout?.inputs[1]?.label}:
           <input
             required
             type="text"
             id="first_name"
             name="first_name"
             bind:value={personal_data.first_name}
+            placeholder={translation?.checkout?.inputs[1]?.placeholder}
             class="px-8 py-4 border-solid outline-none text-base w-full border font-normal border-[var(--color-gray)] rounded-md transition-all duration-300 focus:border-[var(--color-primary-300)];"
           />
         </label>
         <label for="last_name" class="block text-gray-700">
-          Last Name:
+          {translation?.checkout?.inputs[2]?.label}:
           <input
             required
             type="text"
             id="last_name"
             name="last_name"
             bind:value={personal_data.last_name}
+            placeholder={translation?.checkout?.inputs[2]?.placeholder}
             class="px-8 py-4 border-solid outline-none text-base w-full border font-normal border-[var(--color-gray)] rounded-md transition-all duration-300 focus:border-[var(--color-primary-300)];"
           />
         </label>
         <label for="phone" class="block text-gray-700">
-          Phone:
+          {translation?.checkout?.inputs[3]?.label}:
           <input
             required
             type="text"
             id="phone"
             name="phone"
             bind:value={personal_data.phone}
-            placeholder="Add phone number"
+            placeholder={translation?.checkout?.inputs[3]?.placeholder}
             class="px-8 py-4 border-solid outline-none text-base w-full border font-normal border-[var(--color-gray)] rounded-md transition-all duration-300 focus:border-[var(--color-primary-300)];"
           />
         </label>
@@ -106,49 +114,55 @@
     <fieldset
       class="p-6 bg-white rounded-md shadow hover:shadow-lg transition-all duration-200"
     >
-      <legend class="text-2xl font-semibold mb-4">Delivery Address</legend>
+      <legend class="text-2xl font-semibold mb-4"
+        >{translation?.checkout?.delivery_info}</legend
+      >
       <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <label for="country" class="block text-gray-700">
-          Country:
+          {translation?.checkout?.delivery_inputs[0]?.label}:
           <input
             required
             type="text"
             id="country"
             name="country"
             bind:value={delivery_data.country}
+            placeholder={translation?.checkout?.delivery_inputs[0]?.placeholder}
             class="px-8 py-4 border-solid outline-none text-base w-full border font-normal border-[var(--color-gray)] rounded-md transition-all duration-300 focus:border-[var(--color-primary-300)];"
           />
         </label>
         <label for="city" class="block text-gray-700">
-          City:
+          {translation?.checkout?.delivery_inputs[1]?.label}:
           <input
             required
             type="text"
             id="city"
             name="city"
             bind:value={delivery_data.city}
+            placeholder={translation?.checkout?.delivery_inputs[1]?.placeholder}
             class="px-8 py-4 border-solid outline-none text-base w-full border font-normal border-[var(--color-gray)] rounded-md transition-all duration-300 focus:border-[var(--color-primary-300)];"
           />
         </label>
         <label for="postcode" class="block text-gray-700">
-          Postcode:
+          {translation?.checkout?.delivery_inputs[2]?.label}:
           <input
             required
             type="text"
             id="postcode"
             name="postcode"
             bind:value={delivery_data.postcode}
+            placeholder={translation?.checkout?.delivery_inputs[2]?.placeholder}
             class="px-8 py-4 border-solid outline-none text-base w-full border font-normal border-[var(--color-gray)] rounded-md transition-all duration-300 focus:border-[var(--color-primary-300)];"
           />
         </label>
         <label for="address" class="block text-gray-700">
-          Address:
+          {translation?.checkout?.delivery_inputs[3]?.label}:
           <input
             required
             type="text"
             id="address"
             name="address"
             bind:value={delivery_data.address}
+            placeholder={translation?.checkout?.delivery_inputs[3]?.placeholder}
             class="px-8 py-4 border-solid outline-none text-base w-full border font-normal border-[var(--color-gray)] rounded-md transition-all duration-300 focus:border-[var(--color-primary-300)];"
           />
         </label>
@@ -158,7 +172,9 @@
     <fieldset
       class="p-6 bg-white rounded-md shadow max-h-[400px] overflow-y-auto hover:shadow-lg transition-all duration-200"
     >
-      <legend class="text-2xl font-semibold mb-4">Cart Items</legend>
+      <legend class="text-2xl font-semibold mb-4"
+        >{translation?.checkout?.cart}</legend
+      >
       <ul class="space-y-4">
         {#each cart as item}
           <li class="flex items-center space-x-4 border-b pb-4">
@@ -169,10 +185,15 @@
             />
             <div class="flex-grow">
               <p class="font-bold">{item.name}</p>
-              <p class="text-sm text-gray-600">Quantity: {item.amount}</p>
-              <p class="text-sm text-gray-600">Price: ${item.price.regular}</p>
               <p class="text-sm text-gray-600">
-                total: ${item.price.regular * item.amount}
+                {translation?.checkout?.quantity}: {item.amount}
+              </p>
+              <p class="text-sm text-gray-600">
+                {translation?.checkout?.price}: ${item.price.regular}
+              </p>
+              <p class="text-sm text-gray-600">
+                {translation?.checkout?.total_price}: ${item.price.regular *
+                  item.amount}
               </p>
             </div>
           </li>
@@ -183,16 +204,18 @@
     <fieldset
       class="p-6 bg-white rounded-md shadow hover:shadow-lg transition-all duration-200"
     >
-      <legend class="text-2xl font-semibold">Pricing</legend>
+      <legend class="text-2xl font-semibold"
+        >{translation?.checkout?.pricing}</legend
+      >
       <p class="text-lg">
-        <span class="font-semibold">Total Price:</span>
+        <span class="font-semibold">{translation?.checkout?.total_price}:</span>
         ${account.specialist ? price.specialist : price.regular}
       </p>
     </fieldset>
 
     <button
       class="w-48 h-14 self-end bg-black text-white rounded hover:scale-x-105 hover:bg-[var(--color-primary-300)] transition-all"
-      type="submit">Submit</button
+      type="submit">{translation?.checkout?.btn}</button
     >
   </form>
 </section>
--- src/routes/(checkout)/checkout/+page.svelte
+++ src/routes/(checkout)/checkout/+page.svelte
@@ -1,10 +1,20 @@
 <script>
   import Checkout from '$lib/components/sections/checkout/checkout.svelte';
+  import { language } from '$lib/context/store.js';
   export let data;
+
+  $: {
+    if ($language) {
+      translation = $language.langFile;
+    }
+  }
+
+  export let translation;
 </script>
 
 <Checkout
   account={data.account}
   cart={data.cart.products}
   price={data.cart.price}
+  {translation}
 />
