c6751269e6f9124c5f52eb221103d77507adb6a0
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-12-09 12:21:32 +0100

    refactor: started general html cleanup

--- src/lib/components/layouts/footer.svelte
+++ src/lib/components/layouts/footer.svelte
@@ -7,79 +7,71 @@
   export let translation;
 </script>
 
-<section class="py-12 bg-[var(--color-white100)]">
-  <div class="container">
-    <div class="flex max-lg:flex-col lg:justify-between max-lg:gap-5">
-      <!-- <h2 class="text-[var(--color-gray800)] font-medium text-4xl">
-        {translation?.footer?.info?.title}
-      </h2>
-
-      <form class="flex">
-        <input
-          type="email"
-          required
-          autocomplete="email"
-          name="input"
-          id="email"
-          placeholder={translation?.footer?.info?.placeholder}
-          class="px-8 py-4 max-md:w-full cursor-pointer outline-none"
-        />
-        <button
-          type="submit"
-          name="button"
-          class="px-8 py-4 justify-center flex items-center bg-[var(--color-black)] text-[var(--color-white)] hover:bg-[var(--color-gray800)] transition-all duration-300 hover:scale-x-105"
-          >{translation?.footer?.info?.btn}</button
-        >
-      </form>
+<footer class="bg-[var(--color-white100)] p-8 sm:p-12">
+  <div class="container grid grid-cols-1 sm:grid-cols-3 items-center gap-8">
+    <div class="flex flex-col items-start">
+      <a href="/" aria-label="Home">
+        <img src="/svg/logo.svg" alt="Website Logo" class="w-48 h-24" />
+      </a>
+      <p class="text-[var(--color-gray200)] font-normal text-lg">
+        {translation?.footer?.info?.sub_title}
+      </p>
     </div>
-    <div class="py-12">
-      <hr class="border border-[var(--color-gray)]" />
-    </div> -->
-      <div
-        class="grid md:grid-cols-3 max-md:gap-5 items-center md:place-items-center"
+
+    <nav aria-labelledby="footer-navigation" class="flex flex-col">
+      <h2
+        id="footer-navigation"
+        class="text-[var(--color-gray200)] font-semibold text-xl mb-4"
       >
-        <div class="flex flex-gap gap-5">
-          <a href="/">
-            <img src="/svg/logo.svg" alt="logo icon" />
+        {translation?.footer?.info?.link_title}
+      </h2>
+      <ul class="space-y-2">
+        {#each menuItems as element}
+          <li>
+            <a
+              class="text-[var(--color-gray200)] hover:underline text-lg"
+              href={element.href}
+            >
+              {element.label}
+            </a>
+          </li>
+        {/each}
+      </ul>
+    </nav>
 
-            <p class="text-[var(--color-gray200)] font-normal text-xl">
-              {translation?.footer?.info?.sub_title}
-            </p>
+    <address class="not-italic flex flex-col">
+      <h2 class="text-[var(--color-gray200)] font-semibold text-xl mb-4">
+        {translation?.footer?.info?.second_link_title}
+      </h2>
+      <a
+        href="mailto:maximumstyle@email.com"
+        class="text-[var(--color-gray200)] text-lg hover:underline"
+      >
+        maximumstyle@email.com
+      </a>
+      <a
+        href="tel:+123456789"
+        class="text-[var(--color-gray200)] text-lg hover:underline mt-2"
+      >
+        +123456789
+      </a>
+      <ul class="flex space-x-4 mt-4">
+        <li>
+          <a href="/" aria-label="Facebook">
+            <img src="/svg/facebook.svg" alt="Facebook" class="w-6 h-6" />
           </a>
-        </div>
-        <div class="flex flex-col gap-5">
-          <p class="text-[var(--color-gray200)] font-normal text-2xl">
-            {translation?.footer?.info?.link_title}
-          </p>
-
-          <div class="flex flex-col gap-4">
-            {#each menuItems as element}
-              <a class="underline" href={element.href}>
-                {element.label}
-              </a>
-            {/each}
-          </div>
-        </div>
-        <div class="flex flex-col gap-5">
-          <p class="text-[var(--color-gray200)] font-normal text-2xl">
-            {translation?.footer?.info?.second_link_title}
-          </p>
-
-          <a href="mailto:maximumstyle@email.com">maximumstyle@email.com</a>
-          <a href="tel:+123456789">+123456789</a>
-          <div class="flex gap-4">
-            <a href="/">
-              <img src="/svg/facebook.svg" alt="facebook icon" />
-            </a>
-            <a href="/">
-              <img src="/svg/twitter.svg" alt="twitter icon" />
-            </a>
-            <a href="/">
-              <img src="/svg/insta.svg" alt="insta icon" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+        </li>
+        <li>
+          <a href="/" aria-label="Twitter">
+            <img src="/svg/twitter.svg" alt="Twitter" class="w-6 h-6" />
+          </a>
+        </li>
+        <li>
+          <a href="/" aria-label="Instagram">
+            <img src="/svg/insta.svg" alt="Instagram" class="w-6 h-6" />
+          </a>
+        </li>
+      </ul>
+    </address>
   </div>
-</section>
+</footer>
--- src/lib/components/sections/cooperation.svelte
+++ src/lib/components/sections/cooperation.svelte
@@ -3,20 +3,18 @@
 </script>
 
 <section class="py-12 bg-[var(--color-purple)]">
-  <div class="container">
-    <div class="grid max-md:gap-8 md:grid-cols-3 place-items-center">
-      <img src="/svg/jeuDerm.svg" alt="jeuDerm icon" />
-      <img src="/svg/logo.svg" alt="logo icon" />
+  <ul class="container grid max-md:gap-8 md:grid-cols-3 place-items-center">
+    <li><img src="/svg/jeuDerm.svg" alt="jeuDerm icon" /></li>
+    <li><img src="/svg/logo.svg" alt="logo icon" /></li>
 
-      <div class="flex flex-col items-center gap-2 md:gap-1 cursor-default">
-        <h2 class="text-[var(--color-gray800)] text-3xl font-bold">
-          {translation?.main?.cooperation?.name_surname}
-        </h2>
-        <hr class="border border-[var(--color-gray800)] w-14" />
-        <p class="text-[var(--color-gray200)] font-normal text-lg">
-          {translation?.main?.cooperation?.job_title}
-        </p>
-      </div>
-    </div>
-  </div>
+    <li class="flex flex-col items-center gap-2 md:gap-1 cursor-default">
+      <h2 class="text-[var(--color-gray800)] text-3xl font-bold">
+        {translation?.main?.cooperation?.name_surname}
+      </h2>
+      <hr class="border border-[var(--color-gray800)] w-14" />
+      <p class="text-[var(--color-gray200)] font-normal text-lg">
+        {translation?.main?.cooperation?.job_title}
+      </p>
+    </li>
+  </ul>
 </section>
